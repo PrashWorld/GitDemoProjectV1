f59759452246ebe020d73ec2ede217c9eebf4b68
Prashant <prash9650@gmail.com>
2023-12-06 14:20:50 +0530

    first commit

---
+++ src/package1/Test1.java
@@ -0,0 +1,17 @@
+package package1;
+
+public class Test1 {
+	
+	private static void main(String[] args) {
+		
+		String hungary ="yes";
+		if(hungary=="yes") {
+			System.out.println("eat something...");
+		}else {
+			System.out.println("Do work...");
+		}
+		
+
+	}
+
+}
